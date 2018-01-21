812dece5b65713db25377113fc95caae4b5f447e
Nicolas Bloj <nicolasbloj@gmail.com>
2018-01-21 15:19:15 -0300

    express server basic config and test routes

--- api/app.js
+++ api/app.js
@@ -1,1 +1,30 @@
 'use strict'
+
+// Manejo de rutas.
+var express = require('express');
+
+// Conversion de body de las peticiones en obj js.
+var bodyParser = require('body-parser');
+
+// load framework
+var app = express();
+
+// cargar rutas
+
+
+// middlewares
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+// cors
+
+
+// rutas
+app.get('/',(req,res)=>{
+    res.status(200).send({
+        message: 'Hello'
+    });
+});
+
+// exportar
+module.exports = app;
--- api/index.js
+++ api/index.js
@@ -1,12 +1,22 @@
 'use strict'
 
 var mongoose = require('mongoose');
+var app = require('./app');
+var port = 3800;
 
+// connection datbase
 mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/social-net')
+    .then(() => {
+        console.log("Conexion realizada correctamente...");
 
-mongoose.connect('mongodb://localhost:27017/social-net').then(() => console.log("Conexion realizada correctamente..."))
+        //create server
+        app.listen(port, () => {
+            console.log("Servidor creado correctamente, corriendo en http://localhost:3800");
+        });
+
+    })
     .catch((err) => console.log(err));
-
 
 
 
