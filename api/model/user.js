ddb7481fd872ffeceb2891a80701c3da5b0e061a
Nicolas Bloj <nicolasbloj@gmail.com>
2018-01-22 21:47:08 -0300

    user model

---
+++ api/model/user.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = Schema({
+    name: String,
+    surname: String,
+    nick: String,
+    mail: String,
+    password: String,
+    role: String,
+    image: String,
+});
+//_id generado automaticamente .
+
+module.exports = mongoose.model('User',UserSchema);
+// Se crearà en DB collection "users"
